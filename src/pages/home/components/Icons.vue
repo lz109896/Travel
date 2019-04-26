f387d9ca9001dabdee5bf2086aaa5bb5b6c12491
lz109896 <1098969442@qq.com>
2019-04-26 15:06:24 +0800

    add icons

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,94 +1,127 @@
 <template>
   <div class="icons">
-    <div class="icon">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+       <div
+       class="icon"
+       v-for="item of page"
+       :key="item.id">
         <div class='icon-img'>
-            <img class='icon-img-content' src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" />
+            <img class='icon-img-content' :src="item.imgUrl" />
         </div>
-        <p class="icon-desc">热门景点</p>
+        <p class="icon-desc">{{item.desc}}</p>
     </div>
-        <div class="icon">
-        <div class='icon-img'>
-            <img class='icon-img-content' src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/bda58ffc3016edad84e656e8a94b0321.png" />
-        </div>
-        <p class="icon-desc">广州融创</p>
-    </div>
-        <div class="icon">
-        <div class='icon-img'>
-            <img class='icon-img-content' src="http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png" />
-        </div>
-        <p class="icon-desc">深圳必有</p>
-    </div>
-        <div class="icon">
-        <div class='icon-img'>
-            <img class='icon-img-content' src="http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png" />
-        </div>
-        <p class="icon-desc">海洋馆</p>
-    </div>
-        <div class="icon">
-        <div class='icon-img'>
-            <img class='icon-img-content' src="http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png" />
-        </div>
-        <p class="icon-desc">动植物园</p>
-    </div>
-        <div class="icon">
-        <div class='icon-img'>
-            <img class='icon-img-content' src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/cba147cf6cfcea7109d0bff6aac6f626.png" />
-        </div>
-        <p class="icon-desc">深圳动物园</p>
-    </div>
-        <div class="icon">
-        <div class='icon-img'>
-            <img class='icon-img-content' src="http://img1.qunarzz.com/piao/fusion/1803/a6/6d97515091789602.png" />
-        </div>
-        <p class="icon-desc">世界只穿</p>
-    </div>
-        <div class="icon">
-        <div class='icon-img'>
-            <img class='icon-img-content' src="http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png" />
-        </div>
-        <p class="icon-desc">妈呀说公园</p>
-    </div>
+    </swiper-slide>
+  </swiper>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeIcons'
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/bda58ffc3016edad84e656e8a94b0321.png',
+        desc: '广州融创'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0004',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/bda58ffc3016edad84e656e8a94b0321.png',
+        desc: '广州融创'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0006',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/bda58ffc3016edad84e656e8a94b0321.png',
+        desc: '广州融创'
+      }, {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0008',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/bda58ffc3016edad84e656e8a94b0321.png',
+        desc: '广州融创'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0010',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/bda58ffc3016edad84e656e8a94b0321.png',
+        desc: '广州融创'
+      }, {
+        id: '0011',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0012',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/bda58ffc3016edad84e656e8a94b0321.png',
+        desc: '广州融创'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl'
-  .icons
-    overflow: hidden
+  .icons >>> .swiper-container
     height: 0
     padding-bottom: 50%
-    .icon
-      position: relative
+  .icon
+    position: relative
+    overflow: hidden
+    width: 25%
+    float: left
+    height: 0
+    padding-bottom: 25%
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: 0.44rem
+      box-sizing: border-box
+      padding: 0.1rem
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: 0.44rem
+      line-height: 0.44rem
+      text-align: center
+      color: $darkTextColor
       overflow: hidden
-      width: 25%
-      float: left
-      height: 0
-      padding-bottom: 25%
-      .icon-img
-        position: absolute
-        top: 0
-        left: 0
-        right: 0
-        bottom: 0.44rem
-        box-sizing: border-box
-        padding: 0.1rem
-        .icon-img-content
-          display: block
-          margin: 0 auto
-          height: 100%
-      .icon-desc
-        position: absolute
-        left: 0
-        right: 0
-        bottom: 0
-        height: 0.44rem
-        line-height: 0.44rem
-        text-align: center
-        color: $darkTextColor
+      white-space: nowrap
+      text-overflow:list-separator
 </style>
